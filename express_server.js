a647ee1cca8ae51dbb741d720b9d4bd2f5633ba3
Tapan Siwach <siwach.tapan@gmail.com>
2021-08-30 16:57:15 -0400

    add a get route to show the url submission form

--- express_server.js
+++ express_server.js
@@ -18,13 +18,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
