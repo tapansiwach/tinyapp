3a21384d79a0a29d1fa3a202800a2681d8a1c5ca
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-01 19:39:26 -0400

    write unit tests for helper function findUserByEmail

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    assert.equal(user.id, expectedOutput);
+  });
+
+  it('should return undefined if email is not found', function() {
+    const user = findUserByEmail("unknown@example.com", testUsers);
+    const expectedOutput = undefined;
+    assert.equal(user, expectedOutput);
+  });
+
+});
